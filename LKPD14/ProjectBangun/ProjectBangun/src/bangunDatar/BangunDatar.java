4552f734f5f7353e689d7d2e2147ebf2b2511533
Galih-Arvianto <72035766+Galih-Arvianto@users.noreply.github.com>
2021-12-05 20:01:32 +0700

    LKPD 14
    Halo thank you

---
+++ LKPD14/ProjectBangun/ProjectBangun/src/bangunDatar/BangunDatar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bangunDatar;
+
+/**
+ *
+ * @author galih
+ */
+public abstract class BangunDatar {
+
+    public abstract double hitungLuas();
+
+    public abstract double hitungKeliling();
+
+    public void tampilHasil() {
+        System.out.println("Luasnya     :" + this.hitungLuas() + " cm");
+        System.out.println("Kelilingnya :" + this.hitungKeliling() + " cm");
+    }
+}
